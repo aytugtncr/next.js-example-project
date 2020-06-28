d80704f6c96890245e6c111e1ba1838136b84167
aytugtncr <aytugtuncr@gmail.com>
2020-06-28 10:21:52 +0300

    denemeler-1

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,20 @@
+import Head from 'next/head'
+import Nav from '../components/layout'
 function HomePage() {
-    return <div>
-      <h3>Welcome to Next.js!</h3>
-      <p>Bu zeit deploy denemesidir ! </p>
-      
-    </div>
-    
+  return (
+    <div>
+      <Head>
+        <title>Tncr Script</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />       
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      </Head>
 
-  }
+
+    <Nav/>
+
+    </div>
+  )
+}
   
   export default HomePage
